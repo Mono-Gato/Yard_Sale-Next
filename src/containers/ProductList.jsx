e124d8c4e753f7abc0522adc479de8c7d42cf9a8
Monogato <monogato.dg@gmail.com>
2022-09-21 17:11:26 -0500

    FIX SSL URL API

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -4,7 +4,7 @@ import { ProductItem } from '@components/ProductItem';
 import styles from '@styles/ProductList.module.scss';
 import { useGetProducts } from '@hooks/useGetProducts';
 
-const urlAPI = 'http://api.escuelajs.co/api/v1/products';
+const urlAPI = 'https://api.escuelajs.co/api/v1/products';
 
 function ProductList() {
   const products = useGetProducts(urlAPI);
