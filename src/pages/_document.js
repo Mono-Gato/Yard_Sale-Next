0600d56f5890f163fb5c873d662101bff3077e7b
Monogato <monogato.dg@gmail.com>
2022-09-22 15:01:20 -0500

    FIX Build PWA

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,8 +6,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charset="UTF-8" />
-          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          
           {/*Manifest*/}
           <link rel='manifest' href='/manifest.json' />
           {/*Typography*/}
