69e37936b8062ef02c817a663dc03edbc870b0cd
Monogato <monogato.dg@gmail.com>
2022-09-22 13:25:14 -0500

    ADD To PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['api.lorem.space', 'images.pexels.com'],
   },
 };
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+});
+
+module.exports = withPWA(nextConfig);
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,14 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          {/*Manifest*/}
+          <link rel='manifest' href='/manifest.json' />
+          {/*Typography*/}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;500;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
