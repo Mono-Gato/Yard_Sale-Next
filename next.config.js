903706d3673a307432b022ec81ebb48ff53c9f0d
Monogato <monogato.dg@gmail.com>
2022-09-22 21:21:15 -0500

    DELETE public/index.html

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-const runtimeCaching = require("next-pwa/cache");
 
 const nextConfig = {
   reactStrictMode: true,
@@ -13,8 +12,6 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
-  runtimeCaching,
-  buildExcludes: [/middleware-manifest.json$/],
 });
 
 module.exports = withPWA(nextConfig);
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,8 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charset="UTF-8" />
-          
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           {/*Manifest*/}
           <link rel='manifest' href='/manifest.json' />
           {/*Typography*/}
